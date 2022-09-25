8d9133a59d2e6a99f7cd5cbd3c400e3569449d8e
mikaijun <mikai@anyinc.jp>
2022-09-25 13:49:54 +0900

    認証機能の基盤

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { CatsController } from './cats/cats.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, AuthModule, UsersModule],
+  controllers: [AppController],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
